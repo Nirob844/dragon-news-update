81dc4c53f009cfb46db184ce4329852f3502732d
Nirob Hasan <nirob427417@gmail.com>
2023-01-24 09:39:25 +0600

    All news in this category

--- src/Pages/Category/Category/Category.js
+++ src/Pages/Category/Category/Category.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import NewsSummaryCard from '../../Shared/NewsSummaryCart/NewsSummaryCart';
 
 const Category = () => {
 
-    const news = useLoaderData();
-
+    const categoryNews = useLoaderData();
     return (
         <div>
-            <h2>This is Category news {news.length}</h2>
+            <h2>This is Category has news: {categoryNews.length}</h2>
+            {
+                categoryNews.map(news => <NewsSummaryCard
+                    key={news._id}
+                    news={news}
+                ></NewsSummaryCard>)
+
+            }
         </div>
     );
 };
