ea762a361ee57e02e0dea63487d576a443ce0b1e
Nirob Hasan <nirob427417@gmail.com>
2023-01-24 08:22:51 +0600

    all news load

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import NewsSummaryCard from '../../Shared/NewsSummaryCart/NewsSummaryCart';
+
+
 
 const Home = () => {
+
+    const allNews = useLoaderData();
+
     return (
         <div>
-            <h2>This is Home Component</h2>
+            <h2>This is Home news {allNews.length}</h2>
+            {
+                allNews.map(news =>
+                    <NewsSummaryCard
+                        key={news._id}
+                        news={news}>
+
+                    </NewsSummaryCard>)
+            }
         </div>
     );
 };
