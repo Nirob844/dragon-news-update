479bcbea4316cb484cbd4f2d6266ee7c28cf7ac2
Nirob Hasan <nirob427417@gmail.com>
2023-01-24 07:09:37 +0600

    setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqQKPzY5eHa3RcoFdVF5rIYlVnP-Hecxw",
+  authDomain: "dragon-nesw.firebaseapp.com",
+  projectId: "dragon-nesw",
+  storageBucket: "dragon-nesw.appspot.com",
+  messagingSenderId: "294480316689",
+  appId: "1:294480316689:web:c50272388f791d815164b0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
